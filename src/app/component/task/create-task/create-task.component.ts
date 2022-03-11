aebe1ba1f65634ae7baa9f79beef183d50efba3b
Estelle <estellehuynen@yahoo.fr>
2022-03-11 12:09:52 +0100

    post qui fonctionne

--- src/app/component/task/create-task/create-task.component.ts
+++ src/app/component/task/create-task/create-task.component.ts
@@ -20,7 +20,7 @@ export class CreateTaskComponent implements OnInit {
 
   taskInsertForm = new FormGroup({
     'entitled': new FormControl( '', [Validators.required, Validators.minLength(1), Validators.maxLength(30)]),
-    'description': new FormControl(null, [Validators.minLength(1), Validators.maxLength(30)]),
+    'description': new FormControl(null, [Validators.minLength(1), Validators.maxLength(100)]),
     'creationDate': new FormControl([Validators.required]),
     'deadLine': new FormControl(null),
     'endDate': new FormControl(null),
@@ -31,7 +31,7 @@ export class CreateTaskComponent implements OnInit {
   onSubmit(){
     if(this.taskInsertForm.valid){
       this.taskToAdd = this.taskInsertForm.value; 
-      this.service.createTask(this.taskToAdd); 
+      this.service.createTask(this.taskToAdd).subscribe(); 
       this.router.navigateByUrl('/taskList'); 
       console.log(this.taskToAdd);
       
